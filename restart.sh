c203605397a5ddae2e7d04e5f63ef78d7d378cca
Xing Shi <shixing19910105@gmail.com>
2019-04-03 17:12:05 -0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 sudo docker rm -f poem
-sudo docker run -d --name poem --runtime=nvidia --network="host" shixing19910105/poem:1.0 /bin/bash -c 'cd /home/poem/sh/ && bash run_all_docker.sh'
+sudo docker run -d --name poem --runtime=nvidia -p 4000:4000 shixing19910105/poem:1.0 /bin/bash -c 'cd /home/poem/sh/ && bash run_all_docker.sh'
